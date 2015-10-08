ffbf4a30329ce763afde3c695b1686b19feb49ce
Thomas Vincent <thomas.vincent@esrf.fr>
2015-10-08 14:29:22 +0200

    Add a small script to build the slides with landslides

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SLIDES="Documentation Test Project"
+BUILD_DIR="build"
+
+pushd $( dirname $0 )
+
+mkdir -p ${BUILD_DIR}
+
+for DIR in ${SLIDES}; do
+    pushd "${DIR}"
+    echo "Build ${DIR}"
+    landslide --embed --destination=../${BUILD_DIR}/${DIR}.html index.rst
+    popd
+done
+
+popd
